99bb71754be026f9c7892d485390ec58249f7f11
Vi <anim.nika@gmail.com>
2020-11-30 22:19:05 +0300

    add form-contact for js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//gallery
 const galleryImage = document.querySelector('.gallery-image');
 galleryImage.addEventListener('click',function(){
     galleryImage.classList.toggle('grayscale');
@@ -9,4 +10,27 @@ galleryImage2.addEventListener('click',function(){
 const galleryImage3 = document.querySelector('.gallery-image-3');
 galleryImage3.addEventListener('click',function(){
     galleryImage3.classList.toggle('grayscale');
+});
+
+const buttonNext = document.querySelector('.gallery-next');
+buttonNext.addEventListener('click', function(){
+    const galleryItem = document.querySelector('.gallery-item');
+    galleryItem.style.marginLeft = '-100%';
+})
+
+//Form Contact
+const formContact = document.querySelector('.form-contact');
+const inputs = formContact.querySelectorAll('input');
+//перебор списка элементов
+inputs.forEach(function(input){
+    input.addEventListener('focus', function(){
+        this.classList.add('active');
+    });
+    input.addEventListener('blur', function(){
+        const inputValue = this.value;
+        if(inputValue === ''){
+            console.log('value=', inputValue);
+            this.classList.remove('active');
+        }
+    });
 });
