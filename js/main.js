fac80f6d44d7287d54a454b47a00ebdea294ab8d
Vi <anim.nika@gmail.com>
2020-11-30 18:45:53 +0300

    add galleryImage2 and galleryImage3

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ const galleryImage = document.querySelector('.gallery-image');
 galleryImage.addEventListener('click',function(){
     galleryImage.classList.toggle('grayscale');
 });
-
-
+const galleryImage2 = document.querySelector('.gallery-image-2');
+galleryImage2.addEventListener('click',function(){
+    galleryImage2.classList.toggle('grayscale');
+});
+const galleryImage3 = document.querySelector('.gallery-image-3');
+galleryImage3.addEventListener('click',function(){
+    galleryImage3.classList.toggle('grayscale');
+});
